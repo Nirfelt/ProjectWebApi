56c3ec7816c0a1754bd53699e010909ad57e6ec3
Nirfelt <sebastian.nirfelt@gmail.com>
2015-02-21 16:06:55 +0100

    Added func to search for user on github

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import Flask, render_template, request, jsonify
 
-from api_tools import get_profile
+from api_tools import get_profile, get_search_github
 
 app = Flask(__name__)
 
@@ -13,6 +13,12 @@ def index():
 @app.route('/api/v1/search')
 def search_api():
 	query = request.args.get('name')
+	search = get_search_github(query, as_dict = True)
+	return jsonify(search = search)
+
+@app.route('/api/v1/login_github')
+def login_github__api():
+	query = request.args.get('login')
 	profile = get_profile(query)
 	return jsonify(profile = profile.__dict__)
 
--- api_tools.py
+++ api_tools.py
@@ -6,6 +6,10 @@ class Profile(object):
 		self.name = json['name']
 		self.email = json['email']
 
+class User_github(object):
+	def __init__(self, json):
+		self.login = json['login']
+
 def get_profile(query, as_dict = False):
 	profile = None
 
@@ -19,3 +23,19 @@ def get_profile(query, as_dict = False):
 		return profile.__dict__
 	else:
 		return profile
+
+def get_search_github(query, as_dict = False):
+	search = []
+	j = urlopen('https://api.github.com/search/users?q=' + urlencode(query))
+	search_data = json.load(j)
+
+	if len(search_data['items']) > 0:
+
+		for item in search_data['items'][:10]:
+
+			if as_dict:
+				search.append(User_github(item).__dict__)
+			else:
+				search.append(User_github(item))
+
+	return search
