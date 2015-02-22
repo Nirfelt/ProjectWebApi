81e68ac68ed61d591bbffe9ff14c18e1453caa75
Nirfelt <sebastian.nirfelt@gmail.com>
2015-02-22 06:14:08 +0100

    Added func to get github user url

--- api_tools.py
+++ api_tools.py
@@ -1,5 +1,9 @@
 import json
+import urllib2
 from urllib import urlopen, quote_plus as urlencode
+from flask import Flask, request
+
+from flask_mime import Mime
 
 class Profile(object):
 	def __init__(self, json):
@@ -9,6 +13,7 @@ class Profile(object):
 class User_github(object):
 	def __init__(self, json):
 		self.login = json['login']
+		self.test = json['text_matches'][0]['object_url']
 
 def get_profile(query, as_dict = False):
 	profile = None
@@ -26,16 +31,18 @@ def get_profile(query, as_dict = False):
 
 def get_search_github(query, as_dict = False):
 	search = []
-	j = urlopen('https://api.github.com/search/users?q=' + urlencode(query))
+	url = 'https://api.github.com/search/users?q=' + urlencode(query) + '&page=1&per_page=100'
+	headers = {'Accept': 'application/vnd.github.v3.text-match+json'}
+	req = urllib2.Request(url, None, headers=headers)
+	j = urllib2.urlopen(req)
 	search_data = json.load(j)
 
 	if len(search_data['items']) > 0:
 
-		for item in search_data['items'][:10]:
+		for item in search_data['items']:
 
 			if as_dict:
 				search.append(User_github(item).__dict__)
 			else:
 				search.append(User_github(item))
-
 	return search
