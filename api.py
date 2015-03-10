a56cad3b447606e9180c6214c14d64368dfdbf8d
Sebastian Nirfelt <sebastian.nirfelt@gmail.com>
2015-03-10 10:14:43 +0100

    Stackoverflow to api.py

--- api.py
+++ api.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 from flask import Flask, render_template, request, jsonify
 
-from api_tools import get_profile, get_search_github, get_github_user
+from api_tools import get_profile, get_search_github, get_github_user, get_stackoverflow_user
 
 app = Flask(__name__)
 
@@ -12,13 +12,13 @@ def index():
 #API
 @app.route('/api/v1/search')
 def search_api():
-	query = request.args.get('name')
+	query = request.args.get('q')
 	search = get_search_github(query, as_dict = True)
 	return jsonify(search = search)
 
 @app.route('/api/v1/login_github')
 def login_github__api():
-	query = request.args.get('login')
+	query = request.args.get('q')
 	profile = get_profile(query)
 	return jsonify(profile = profile.__dict__)
 
@@ -26,6 +26,12 @@ def login_github__api():
 def user_github__api():
 	query = request.args.get('q')
 	profile = get_github_user(query)
+	return jsonify(profile = profile.__dict__)
+
+@app.route('/api/v1/user_stackoverflow')
+def user_stackoverflow__api():
+	query = request.args.get('q')
+	profile = get_stackoverflow_user(query)
 	return jsonify(profile = profile.__dict__)
 
 if __name__ == "__main__":
